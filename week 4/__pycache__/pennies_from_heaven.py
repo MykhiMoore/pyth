d7bbd9fc3023752d4d645fb2f5988eeea9a4d98d
MykhiMoore <mykhimoore1004@gmail.com>
2022-09-20 13:02:26 -0400

    changes

--- week 4/__pycache__/pennies_from_heaven.py
+++ week 4/__pycache__/pennies_from_heaven.py
@@ -13,7 +13,7 @@ print(("dollars") + str(dollars))
 print(("quarters") + str(quarters))
 print(("dimes") + str (dimes))
 print(("nickels") + str (nickels))
-print(("pennies") + str(pennies))s
+print(("pennies") + str(pennies)) 
 
 
 
