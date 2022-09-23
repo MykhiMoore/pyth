1e7146264a340678d2b7835d17102c050923d062
MykhiMoore <mykhimoore1004@gmail.com>
2022-09-23 11:47:41 -0400

    23/09/22

---
+++ week 2/test_3.py
@@ -0,0 +1,16 @@
+playstation_5 = 549.99
+xbox_seies_x = 499.99
+nintendo_switch = 349.99
+oculus_quest_2 = 399.99
+
+print(f"The most expensive console is {playstation_5}")
+print(f"The least expensive console is {nintendo_switch}")
+roundplaystation = round(playstation_5)
+roundxbox = round(xbox_seies_x)
+roundnintendo = round(nintendo_switch)
+roundoculus = round(oculus_quest_2)
+print(f"the rounded price for the playstation 5 is {roundplaystation}")
+print(f"the rounded price for  the xbox series x is {roundxbox}")
+print(f"the rounded price for the nintendo switch is {roundnintendo}")
+print(f"the rounded price for the oculus quest 2 is {roundoculus}")
+print(f"The average price of all consoles is {playstation_5 + xbox_seies_x + nintendo_switch + oculus_quest_2}")
---
+++ week 5/lists.py
@@ -0,0 +1,19 @@
+shopping_list = ["steak", "chicken", "apples", "fries"]
+# index =           0        1           2        3
+# lists are indexed from 0 ^^^^^^^^
+print(shopping_list)
+print(shopping_list[1])
+items_2 = shopping_list[1]
+print(items_2)
+
+def my_function():
+        print("sam")
+joel = my_function()
+functiion_list = [joel]
+functiion_list[0]
+
+
+embedded_lists = [[["Number of Cars", 3],["Car Brands", "Chevy", "Toyota", "Honda"],["MPG", 18, 35.5, 12]],[["Protein", "Veggies", "Rice", "Cheese"],["Chicken", "Steak", "Pork"], ["Peppers", "Onions", "Tomatoes", "Corn"],["White", "Brown"], ["Colby", "Swiss"]]]
+
+
+print(embedded_lists[0][1][2], embedded_lists[0][2][2], embedded_lists[1][1][0], embedded_lists[1][4][1])
